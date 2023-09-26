19023c6438c309b0ad5a17a32a99dc0de225453c
Evan Gorman <evan.gorman@colorado.edu>
2023-09-25 19:57:13 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 25 18:58:53 2023
+
+@author: Evan
+"""
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='Haar-Like-Metric-Learning',
+    url='https://github.com/edgor17/Sparsify-Ultrametric',
+    author='Evan Gorman',
+    author_email='evan.gorman@colorado.edu',
+    packages=['Haar-Like-Metric-Learning'],
+    install_requires=['numpy','pandas','ete3','scipy','matplotlib','sklearn','seaborn'],
+    version='0.1',
+    description='Adaptive Metric Learning for Metagenomics',
+    long_description=long_description
+)
